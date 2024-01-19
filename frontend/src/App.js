3c3efdb733f5d8f277a5758c185af3573f7de70d
Simão Moreno <simaojmoreno@gmail.com>
2024-01-19 17:56:10 +0000

    Some comments removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,8 @@ function App() {
   return (
     // A div with class "App" is the main container
     <div className="App">
-      // The header of the App contains the logo and a message
       <header className="App-header">
-        // The logo is an image with source "csmi-logo.png" and alt text "logo"
         <img src="csmi-logo.png" className="" alt="logo" />
-        // The message is a paragraph saying "Brevemente!"
         <p>
           Brevemente!
         </p>
