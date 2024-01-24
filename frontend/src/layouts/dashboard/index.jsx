57f46acb4c21505bc04dc36572727e78e3b5a7a2
Simão Moreno <simaojmoreno@gmail.com>
2024-01-24 13:05:50 +0000

    Refactor DashboardLayout

--- frontend/src/layouts/dashboard/index.jsx
+++ frontend/src/layouts/dashboard/index.jsx
@@ -10,12 +10,16 @@ import Header from './header';
 // ----------------------------------------------------------------------
 
 export default function DashboardLayout({ children }) {
+
+  // Estado para controlar a abertura da navegação
   const [openNav, setOpenNav] = useState(false);
 
   return (
     <>
+      {/* Cabeçalho, que recebe uma função para abrir a navegação */}
       <Header onOpenNav={() => setOpenNav(true)} />
 
+      {/* Box que contém a navegação e o conteúdo principal */}
       <Box
         sx={{
           minHeight: 1,
@@ -23,14 +27,17 @@ export default function DashboardLayout({ children }) {
           flexDirection: { xs: 'column', lg: 'row' },
         }}
       >
+        {/* Navegação, que recebe o estado de abertura e uma função para fechá-la */}
         <Nav openNav={openNav} onCloseNav={() => setOpenNav(false)} />
 
+        {/* Conteúdo principal, que recebe os filhos do componente DashboardLayout */}
         <Main>{children}</Main>
       </Box>
     </>
   );
 }
 
+// Definindo as propriedades do componente
 DashboardLayout.propTypes = {
   children: PropTypes.node,
 };
