c3bfc1070dda26bac93ec94eca568fd82e3f2e38
Simão Moreno <simaojmoreno@gmail.com>
2024-01-24 12:46:48 +0000

    Refactor config-navigation.jsx

--- frontend/src/layouts/dashboard/config-navigation.jsx
+++ frontend/src/layouts/dashboard/config-navigation.jsx
@@ -1,11 +1,18 @@
+// Importando o componente SvgColor
 import SvgColor from 'src/components/svg-color';
 
-// ----------------------------------------------------------------------
-
+// Função para criar um ícone SVG com uma cor específica
+// O nome do ícone é passado como argumento
 const icon = (name) => (
+  // SvgColor é um componente que renderiza um ícone SVG
+  // O caminho para o arquivo SVG é construído usando o nome do ícone
+  // A largura e a altura do ícone são definidas como 1
   <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
 );
 
+// Configuração da navegação
+// Cada item do array representa um link na navegação
+// Cada item tem um título, um caminho e um ícone
 const navConfig = [
   {
     title: 'dashboard',
@@ -39,4 +46,5 @@ const navConfig = [
   },
 ];
 
+// Exportando a configuração da navegação
 export default navConfig;
